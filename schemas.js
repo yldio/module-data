a87f34849287feec5099e67ca799abe9479ddffe
ricardofbarros <ricardofbarros@hotmail.com>
2016-05-15 19:58:04 +0100

    specify vulnerabilities schema obj

--- schemas.js
+++ schemas.js
@@ -1,9 +1,16 @@
 var Joi = require('joi')
 var spdxLicenseIds = require('spdx-license-ids')
-var knownTestFrameworks = require('./lib/local/tests').testFrameworks
+var knownTestFrameworks = require('./lib/api/local/tests').testFrameworks
 
 var versionRegex = /([0-9]+)\.([0-9]+)\.([0-9]+)(?:(\-[0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-\-\.]+)?/g
 var mdlRegex = /(\w|\d)+@([0-9]+)\.([0-9]+)\.([0-9]+)(?:(\-[0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-\-\.]+)?/g
+
+var vulnerabilitiesObj = Joi.object().keys({
+  id: Joi.string().required(),
+  url: Joi.string().uri({
+    scheme: ['http', 'https']
+  }).required()
+})
 
 var remoteDataSchema = Joi.object().keys({
   name: Joi.string().required(),
@@ -22,7 +29,7 @@ var remoteDataSchema = Joi.object().keys({
       isOutdated: Joi.boolean().required(),
       isDeprecated: Joi.boolean().required(),
       vulnerabilities: Joi.array().required()
-        .items(Joi.object())
+        .items(vulnerabilitiesObj)
     })
   ),
   __moduleData: Joi.object().keys({
@@ -74,7 +81,7 @@ var standardDataSchema = Joi.object().keys({
   isOutdated: Joi.boolean().required(),
   isDeprecated: Joi.boolean().required(),
   vulnerabilities: Joi.array().required()
-    .items(Joi.object()),
+    .items(vulnerabilitiesObj),
   __moduleData: Joi.object().keys({
     version: Joi.string().regex(versionRegex).required(),
     type: Joi.string().valid('standard').required()
